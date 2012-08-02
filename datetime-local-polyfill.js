6628c9a25972c8110e6faf80416eb3c317ce162b
Jonathan Stipe <JonStipe@prodigy.net>
2012-08-02 10:03:20 -0700

    Measure step from epoch.

--- datetime-local-polyfill.js
+++ datetime-local-polyfill.js
@@ -174,25 +174,15 @@
         if (step !== undefined && step != 'any') {
           var kNum = inDate.getTime();
           var raisedStep = step * 1000;
-          if (min !== undefined) {
-            var minNum = min.getTime();
-            var stepDiff = (kNum - minNum) % raisedStep;
-            var stepDiff2 = raisedStep - stepDiff;
-            if (stepDiff == 0) return inDate;
-            else {
-              if (stepDiff > stepDiff2) return new Date(inDate.getTime() + stepDiff2);
-              else return new Date(inDate.getTime() - stepDiff);
-            }
-          } else if (max !== undefined) {
-            var maxNum = max.getTime();
-            var stepDiff = (maxNum - kNum) % raisedStep;
-            var stepDiff2 = raisedStep - stepDiff;
-            if (stepDiff == 0) return inDate;
-            else {
-              if (stepDiff > stepDiff2) return new Date(inDate.getTime() - stepDiff2);
-              else return new Date(inDate.getTime() + stepDiff);
-            }
-          } else return inDate;
+          if (min === undefined) min = new Date(0);
+          var minNum = min.getTime();
+          var stepDiff = (kNum - minNum) % raisedStep;
+          var stepDiff2 = raisedStep - stepDiff;
+          if (stepDiff == 0) return inDate;
+          else {
+            if (stepDiff > stepDiff2) return new Date(inDate.getTime() + stepDiff2);
+            else return new Date(inDate.getTime() - stepDiff);
+          }
         } else return inDate;
       }
       $('input[type="datetime-local"]').each(function(index) {
